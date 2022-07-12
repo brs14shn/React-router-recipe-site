70734a00f146786a048b02ac2cdbf91604cc5bde
brs14shn <brsshn292929@gmail.com>
2022-07-12 18:21:53 +0300

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import AppRouter from './router/AppRouter';
-import {BrowserRouter} from "react-router-dom"
+import {HashRouter} from "react-router-dom"
 
 
 function App() {
   return (
       
-  <BrowserRouter>
+  <HashRouter>
     <AppRouter/>
-  </BrowserRouter>
+  </HashRouter>
     
 
  
